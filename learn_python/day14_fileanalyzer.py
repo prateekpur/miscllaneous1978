0a018204a11d624dda6880afb1a7dd994c6cc49c
Prateek Puri <prateekpuri@yahoo.com>
2026-05-08 13:48:36 -0400

    Day14 implementation of csv file summarizer

---
+++ learn_python/day14_fileanalyzer.py
@@ -0,0 +1,61 @@
+#coloms
+sal_col = 4
+age_col = 3
+exp_years_col = 5
+emp_id_col = 0
+name_col = 1
+dept_col = 2
+
+def summary_numeric(lines):
+    tot_salary = 0
+    tmp_val = lines[0].split(",")
+    max_sal = float(tmp_val[sal_col])
+    min_sal = float(tmp_val[sal_col])
+    for line in lines :
+        vals = line.split(",")
+        salary = float(vals[sal_col])
+        #print(salary)
+        if (salary > max_sal) : 
+            max_sal = salary
+        if (salary < min_sal) :
+            min_sal = salary
+        #print ("Max : ", max_sal, "Min : ", min_sal)
+        tot_salary = tot_salary + salary
+    print ("Total : ", tot_salary, "\nAverage : ", tot_salary / len(lines))
+    print ("Max : ", max_sal, "\nMin : ", min_sal)
+
+def dept_wise_count(lines):
+    counts = {}
+    for line in lines :
+        vals = line.split(",")
+        dept = vals[dept_col]
+        counts[dept] = counts.get(dept, 0) + 1
+    return counts
+
+def summary_data(lines):
+    print("Rows : ", len(lines)-1)
+    cols = lines[0].strip().split(",")
+    print("Coloms:" , len(cols))
+    print("Headers : ", lines[0])
+
+def read_file():
+    filename = "employees2.csv"
+    while True:        
+        try:
+            with open(filename, "r") as file:
+                lines = file.readlines()
+            return lines
+        except FileNotFoundError:
+            filename = input("Invalid input, please enter a file name : ")   
+
+
+if __name__ == "__main__":
+    lines = read_file()
+    if len(lines) > 0 :
+        summary_data(lines)
+    
+    if len(lines) <= 1 :
+        print("File has no records of data")
+    else :
+        summary_numeric(lines[1:])
+        print(dept_wise_count(lines[1:]))
